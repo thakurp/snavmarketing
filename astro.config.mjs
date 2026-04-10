6c5e9a5e5836503d8829ddbba0b6a479e32f83fa
Thakur P <thakurp@gmail.com>
2026-04-10 06:49:19 -0700

    config: update site URL to snav.saasapp.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://snavmarketing.com.au',
+  site: 'https://snav.saasapp.net',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
